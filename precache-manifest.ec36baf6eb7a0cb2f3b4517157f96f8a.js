4aba4d8eb3284b950c3b176043a1016464f71efe
dbreznay <60904436+dbreznay@users.noreply.github.com>
2020-10-12 22:22:00 -0500

    Updates

---
+++ precache-manifest.ec36baf6eb7a0cb2f3b4517157f96f8a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d81221e2b0655fb5ee095870ff95642e",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "cf1998262561c8d1d012",
+    "url": "/react-portfolio/static/css/2.cd1c2086.chunk.css"
+  },
+  {
+    "revision": "b1a73cb1e32db7d0497f",
+    "url": "/react-portfolio/static/css/main.2eb585c5.chunk.css"
+  },
+  {
+    "revision": "cf1998262561c8d1d012",
+    "url": "/react-portfolio/static/js/2.9df5fc07.chunk.js"
+  },
+  {
+    "revision": "e1d2b252d73a4c2bc4d6802ca4ba1aa0",
+    "url": "/react-portfolio/static/js/2.9df5fc07.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b1a73cb1e32db7d0497f",
+    "url": "/react-portfolio/static/js/main.7ee5b5f8.chunk.js"
+  },
+  {
+    "revision": "aff33cef9dbb8787d6b3",
+    "url": "/react-portfolio/static/js/runtime-main.213b1880.js"
+  }
+]);
