9bdb90d68faf2269d14816d37e88f7ef0ad0fbd2
dbreznay <60904436+dbreznay@users.noreply.github.com>
2020-10-13 01:20:24 -0500

    added definitions

--- src/components/conact.js
+++ src/components/conact.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Grid, Cell} from 'react-mdl';
+import { Grid, Cell, List, ListItem, ListItemContent } from 'react-mdl';
 
 class Contact extends Component {
     render() {
